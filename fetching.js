a6225b9dedecf3152ecf3255236e4d4f8c6ef462
Adjourner Harbringe <161456818+Adjyharby@users.noreply.github.com>
2025-01-03 17:43:01 +0800

    Update fetching.js

--- fetching.js
+++ fetching.js
@@ -1,4 +1,56 @@
-    // $(".modalbuttons").toggle(1000);
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>2024 Art Folio</title>
+  <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
+  <style>
+    img {
+      max-width: 100px;
+      margin: 5px;
+    }
+  </style>
+</head>
+<body>
+  <div id="loadingMessage">Loading...</div>
+  <div id="january">January</div>
+  <div id="may">May</div>
+  <div id="october">October</div>
+  <div id="december">December</div>
+
+  <button id="februarytoggle">February</button>
+  <div id="febdiv"></div>
+
+  <button id="Marchtoggle">March</button>
+  <div id="mardiv"></div>
+
+  <button id="apriltoggle">April</button>
+  <div id="aprdiv"></div>
+
+  <button id="junetoggle">June</button>
+  <div id="jundiv"></div>
+
+  <button id="julytoggle">July</button>
+  <div id="juldiv"></div>
+
+  <button id="augusttoggle">August</button>
+  <div id="augdiv"></div>
+
+  <button id="septtoggle">September</button>
+  <div id="sepdiv"></div>
+
+  <button id="novtoggle">November</button>
+  <div id="novdiv"></div>
+
+  <button id="selftoggle">Self</button>
+  <div id="selfdiv"></div>
+
+  <button id="commissiontoggle">Commission</button>
+  <div id="commdiv"></div>
+
+  <script>
+    // Hide the loading message
     $("#loadingMessage").animate(
       {
         height: "0px",
@@ -9,438 +61,81 @@
       }
     );
 
-    let prevtext = $("#january").text();
-    document.querySelector("#january").addEventListener("click", () => {
-      let currtext = $("#january").text();
+    // Refactor month toggle functionality
+    function setupMonthToggle(monthId, newText, originalTextTimeout = 3000) {
+      let prevText = $(monthId).text();
 
-      if (currtext === prevtext) {
-        $("#january").toggle(1000, () => {
-          $("#january").html("<h2>" + "I was busy writing the game plot for stranded" + "</h2>");
-        });
+      document.querySelector(monthId).addEventListener("click", () => {
+        let currText = $(monthId).text();
 
-        $("#january").toggle(1000);
-
-        setTimeout(() => {
-          $("#january").toggle(1000, () => {
-            $("#january").html("<h2>" + prevtext + "</h2>");
+        if (currText === prevText) {
+          $(monthId).toggle(1000, () => {
+            $(monthId).html(`<h2>${newText}</h2>`);
           });
 
-          $("#january").toggle(1000);
-        }, 3000);
-      }
-    });
-    let prevtext2 = $("#may").text();
-    document.querySelector("#may").addEventListener("click", () => {
-      let currtext2 = $("#may").text();
+          $(monthId).toggle(1000);
 
-      if (currtext2 === prevtext2) {
-        $("#may").toggle(1000, () => {
-          $("#may").html("<h2>" + "It was the finals for my second year I couldn't risk it" + "</h2>");
-        });
+          setTimeout(() => {
+            $(monthId).toggle(1000, () => {
+              $(monthId).html(`<h2>${prevText}</h2>`);
+            });
 
-        $("#may").toggle(1000);
+            $(monthId).toggle(1000);
+          }, originalTextTimeout);
+        }
+      });
+    }
 
-        setTimeout(() => {
-          $("#may").toggle(1000, () => {
-            $("#may").html("<h2>" + prevtext2 + "</h2>");
+    // Initialize toggles for specific months
+    setupMonthToggle("#january", "I was busy writing the game plot for stranded");
+    setupMonthToggle("#may", "It was the finals for my second year I couldn't risk it");
+    setupMonthToggle("#october", "my month, my rest");
+    setupMonthToggle("#december", "I will start around 25");
+
+    // Function to set up image toggles
+    function setupImageToggle(toggleId, divId, imagePath, imageCount, extraImages = []) {
+      $(`${toggleId}`).click(() => {
+        if ($(`${divId}`).children().length == 0) {
+          $(`${divId}`).toggle(10);
+          let content = "";
+
+          // Add the images from the folder
+          for (let x = 1; x <= imageCount; x++) {
+            content += `<img src='./pictures/${imagePath}${x}.png' alt='Image ${x}'>`;
+          }
+
+          // Add extra images (like GIFs) if provided
+          extraImages.forEach((image) => {
+            content += `<img src='./pictures/${image}' alt='Extra Image'>`;
           });
 
-          $("#may").toggle(1000);
-        }, 3000);
-      }
-    });
-
-    let prevtext3 = $("#october").text();
-    document.querySelector("#october").addEventListener("click", () => {
-      let currtext3 = $("#october").text();
-
-      if (currtext3 === prevtext3) {
-        $("#october").toggle(1000, () => {
-          $("#october").html("<h2>" + "my month, my rest" + "</h2>");
-        });
-
-        $("#october").toggle(1000);
-        setTimeout(() => {
-          $("#october").toggle(1000, () => {
-            $("#october").html("<h2>" + prevtext3 + "</h2>");
+          $(`${divId}`).html(content);
+          $(`${divId}`).toggle(1000, () => {
+            $("#march, #april, #june, #july, #august,#september,#november").toggle(1000);
           });
-
-          $("#october").toggle(1000);
-        }, 3000);
-      }
-    });
-
-    let prevtext4 = $("#december").text();
-    document.querySelector("#december").addEventListener("click", () => {
-      let currtext4 = $("#december").text();
-
-      if (currtext4 === prevtext4) {
-        $("#december").toggle(1000, () => {
-          $("#december").html("<h2>" + "I will start around 25" + "</h2>");
-        });
-
-        $("#december").toggle(1000);
-
-        setTimeout(() => {
-          $("#december")
-            .toggle(1000, () => {
-              $("#december").html("<h2>" + prevtext4 + "</h2>");
-            })
-            .toggle(1000);
-        }, 3000);
-      }
-    });
-
-    $("#februarytoggle").click(() => {
-      if ($("#febdiv").children().length == 0) {
-        $("#febdiv").toggle(10);
-        let content = "";
-        for (let x = 1; x <= 5; x++) {
-          content +=
-            "<img src='" +
-            "/pictures/" +
-            "feb" +
-            x +
-            ".png' alt='Image " +
-            x +
-            "'>";
+        } else {
+          $(`${divId}`).toggle(1000, () => {
+            $(`${divId}`).empty();
+            $("#march, #april, #june, #july, #august,#september,#november").toggle(1000);
+          });
+          $(`${divId}`).toggle(10);
         }
-        $("#febdiv").html(content);
-        $("#febdiv").toggle(1000, () => {
-          $(
-            "#march, #april, #june, #july, #august,#september,#november"
-          ).toggle(1000);
-        });
-      } else {
-        $("#febdiv").toggle(1000, () => {
-          $("#febdiv").empty();
-          $(
-            "#march, #april, #june, #july, #august,#september,#november"
-          ).toggle(1000);
-        });
-        $("#febdiv").toggle(10);
-      }
-    });
+      });
+    }
 
-    $("#Marchtoggle").click(() => {
-      if ($("#mardiv").children().length == 0) {
-        $("#mardiv").toggle(10);
-        let content = "";
-        for (let x = 1; x <= 5; x++) {
-          content +=
-            "<img src='" +
-            "/pictures/" +
-            "mar" +
-            x +
-            ".png' alt='Image " +
-            x +
-            "'>";
-        }
-        $("#mardiv").html(content);
-        $("#mardiv").toggle(1000, () => {
-          $(
-            "#february, #april, #june, #july, #august,#september,#november"
-          ).toggle(1000);
-        });
-      } else {
-        $("#mardiv").toggle(1000, () => {
-          $("#mardiv").empty();
-          $(
-            "#february, #april, #june, #july, #august,#september,#november"
-          ).toggle(1000);
-        });
-        $("#mardiv").toggle(10);
-      }
-    });
+    // Initialize toggles for specific months with images
+    setupImageToggle("#februarytoggle", "#febdiv", "feb", 5);
+    setupImageToggle("#Marchtoggle", "#mardiv", "mar", 5);
+    setupImageToggle("#apriltoggle", "#aprdiv", "apr", 2);
+    setupImageToggle("#junetoggle", "#jundiv", "jun", 2);
+    setupImageToggle("#julytoggle", "#juldiv", "jul", 3, ["jul2.gif", "jul4.gif"]);
+    setupImageToggle("#augusttoggle", "#augdiv", "aug", 5, ["aug1.gif"]);
+    setupImageToggle("#septtoggle", "#sepdiv", "sep", 1);
+    setupImageToggle("#novtoggle", "#novdiv", "nov", 10);
 
-    $("#apriltoggle").click(() => {
-      if ($("#aprdiv").children().length == 0) {
-        $("#aprdiv").toggle(10);
-        let content = "";
-        for (let x = 1; x <= 2; x++) {
-          content +=
-            "<img src='" +
-            "/pictures/" +
-            "apr" +
-            x +
-            ".png' alt='Image " +
-            x +
-            "'>";
-        }
-        $("#aprdiv").html(content);
-        $("#aprdiv").toggle(1000, () => {
-          $(
-            "#february, #march, #june, #july, #august,#september,#november"
-          ).toggle(1000);
-        });
-      } else {
-        $("#aprdiv").toggle(1000, () => {
-          $("#aprdiv").empty();
-          $(
-            "#february, #march, #june, #july, #august,#september,#november"
-          ).toggle(1000);
-        });
-        $("#aprdiv").toggle(10);
-      }
-    });
-
-    $("#junetoggle").click(() => {
-      if ($("#jundiv").children().length == 0) {
-        $("#jundiv").toggle(10);
-        let content = "";
-        for (let x = 1; x <= 2; x++) {
-          content +=
-            "<img src='" +
-            "/pictures/" +
-            "jun" +
-            x +
-            ".png' alt='Image " +
-            x +
-            "'>";
-        }
-        $("#jundiv").html(content);
-        $("#jundiv").toggle(1000, () => {
-          $(
-            "#february, #march, #april, #july, #august,#september,#november"
-          ).toggle(1000);
-        });
-      } else {
-        $("#jundiv").toggle(1000, () => {
-          $("#jundiv").empty();
-          $(
-            "#february, #march, #april, #july, #august,#september,#november"
-          ).toggle(1000);
-        });
-        $("#jundiv").toggle(10);
-      }
-    });
-
-    $("#julytoggle").click(() => {
-      if ($("#juldiv").children().length == 0) {
-        $("#juldiv").toggle(10);
-        let content = "";
-        for (let x = 1; x <= 3; x++) {
-          content +=
-            "<img src='" +
-            "/pictures/" +
-            "jul" +
-            x +
-            ".png' alt='Image " +
-            x +
-            "'>";
-        }
-        content +=
-          "<img src='" +
-          "/pictures/" +
-          "jul" +
-          2 +
-          ".gif' alt='Image " +
-          2 +
-          "'>";
-
-        content +=
-          "<img src='" +
-          "/pictures/" +
-          "jul" +
-          4 +
-          ".gif' alt='Image " +
-          4 +
-          "'>";
-
-        $("#juldiv").html(content);
-        $("#juldiv").toggle(1000, () => {
-          $(
-            "#february, #march, #april, #june, #august,#september,#november"
-          ).toggle(1000);
-        });
-      } else {
-        $("#juldiv").toggle(1000, () => {
-          $("#juldiv").empty();
-          $(
-            "#february, #march, #april, #june, #august,#september,#november"
-          ).toggle(1000);
-        });
-        $("#juldiv").toggle(10);
-      }
-    });
-
-    $("#augusttoggle").click(() => {
-      if ($("#augdiv").children().length == 0) {
-        $("#augdiv").toggle(10);
-        let content = "";
-        for (let x = 1; x <= 5; x++) {
-          content +=
-            "<img src='" +
-            "/pictures/" +
-            "aug" +
-            x +
-            ".png' alt='Image " +
-            x +
-            "'>";
-        }
-        content +=
-          "<img src='" +
-          "/pictures/" +
-          "aug" +
-          1 +
-          ".gif' alt='Image " +
-          1 +
-          "'>";
-
-        $("#augdiv").html(content);
-        $("#augdiv").toggle(1000, () => {
-          $(
-            "#february, #march, #april, #june, #july,#september,#november"
-          ).toggle(1000);
-        });
-      } else {
-        $("#augdiv").toggle(1000, () => {
-          $("#augdiv").empty();
-          $(
-            "#february, #march, #april, #june, #july,#september,#november"
-          ).toggle(1000);
-        });
-        $("#augdiv").toggle(10);
-      }
-    });
-
-    $("#septtoggle").click(() => {
-      if ($("#sepdiv").children().length == 0) {
-        $("#sepdiv").toggle(10);
-
-        content =
-          "<img src='" +
-          "/pictures/" +
-          "sep" +
-          1 +
-          ".png' alt='Image " +
-          1 +
-          "'>";
-
-        $("#sepdiv").html(content);
-        $("#sepdiv").toggle(1000, () => {
-          $("#march, #april, #june, #july, #august,#february,#november").toggle(
-            1000
-          );
-        });
-      } else {
-        $("#sepdiv").toggle(1000, () => {
-          $("#sepdiv").empty();
-          $("#march, #april, #june, #july, #august,#february,#november").toggle(
-            1000
-          );
-        });
-        $("#sepdiv").toggle(10);
-      }
-    });
-
-    $("#novtoggle").click(() => {
-      if ($("#novdiv").children().length == 0) {
-        $("#novdiv").toggle(10);
-        let content = "";
-        for (let x = 1; x <= 10; x++) {
-          content +=
-            "<img src='" +
-            "/pictures/" +
-            "nov" +
-            x +
-            ".png' alt='Image " +
-            x +
-            "'>";
-        }
-        $("#novdiv").html(content);
-        $("#novdiv").toggle(1000, () => {
-          $(
-            "#march, #april, #june, #july, #august,#september,#february"
-          ).toggle(1000);
-        });
-      } else {
-        $("#novdiv").toggle(1000, () => {
-          $("#novdiv").empty();
-          $(
-            "#march, #april, #june, #july, #august,#september,#february"
-          ).toggle(1000);
-        });
-        $("#novdiv").toggle(10);
-      }
-    });
-
-    $("#selftoggle").click(() => {
-      if ($("#selfdiv").children().length == 0) {
-        $("#selfdiv").toggle(10);
-        let content = "";
-        for (let x = 1; x <= 21; x++) {
-          content +=
-            "<img src='" +
-            "/pictures/" +
-            "self/s" +
-            x +
-            ".png' alt='Image " +
-            x +
-            "'>";
-        }
-        content +=
-          "<img src='" +
-          "/pictures/" +
-          "self/s" +
-          1 +
-          ".gif' alt='Image " +
-          1 +
-          "'>";
-
-        $("#selfdiv").html(content);
-        $("#selfdiv").toggle(1000, () => {
-          $("#commission").toggle(1000);
-        });
-      } else {
-        $("#selfdiv").toggle(1000, () => {
-          $("#selfdiv").empty();
-          $("#commission").toggle(1000);
-        });
-        $("#selfdiv").toggle(10);
-      }
-    });
-
-    $("#commissiontoggle").click(() => {
-      if ($("#commdiv").children().length == 0) {
-        $("#commdiv").toggle(10);
-        let content = "";
-        for (let x = 1; x <= 12; x++) {
-          content +=
-            "<img src='" +
-            "/pictures/" +
-            "comms/c" +
-            x +
-            ".png' alt='Image " +
-            x +
-            "'>";
-        }
-        content +=
-          "<img src='" +
-          "/pictures/" +
-          "comms/c" +
-          1 +
-          ".gif' alt='Image " +
-          1 +
-          "'>";
-        content +=
-          "<img src='" +
-          "/pictures/" +
-          "comms/c" +
-          2 +
-          ".gif' alt='Image " +
-          2 +
-          "'>";
-
-        $("#commdiv").html(content);
-        $("#commdiv").toggle(1000, () => {
-          $("#self").toggle(1000);
-        });
-      } else {
-        $("#commdiv").toggle(1000, () => {
-          $("#commdiv").empty();
-          $("#self").toggle(1000);
-        });
-        $("#commdiv").toggle(10);
-      }
-    });
+    // Self and Commission toggles
+    setupImageToggle("#selftoggle", "#selfdiv", "self/s", 21, ["self/s1.gif"]);
+    setupImageToggle("#commissiontoggle", "#commdiv", "comms/c", 12, ["comms/c1.gif", "comms/c2.gif"]);
+  </script>
+</body>
+</html>
